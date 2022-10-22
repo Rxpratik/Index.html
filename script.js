675ad4e5e1ba39881fcb4e7f184b8bc6d9ce64f3
Rxpratik <116332580+Rxpratik@users.noreply.github.com>
2022-10-22 11:15:42 +0530

    Script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+        function openPage() {
+            var x = document.getElementById("search").value;
+
+            if (x === "school bag") {
+                window.open("/School-bag.html");
+            }
+
+            if (x === "traveling bag") {
+                window.open("/index.html");
+            }
+
+        }
